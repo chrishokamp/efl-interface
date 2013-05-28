12637f28f4cf902f13d500f4ac97a12d9653cd73
Chris Hokamp <chris.hokamp@gmail.com>
2013-05-28 15:28:38 +0100

    towards persisting userData on the server

--- cgi/userData.pl
+++ cgi/userData.pl
@@ -4,29 +4,26 @@
 
 use strict;
 use warnings;
-
-#use Encode;
 use CGI '-utf8'; #TODO: ensure that this is necessary
 use CGI::Carp 'fatalsToBrowser';
+#use Encode;
 #use JSON;
 #use Data::Dumper;
 
 my $userData = new CGI; 
 my $jsonString = $userData->param("userData");
-#my $jsonString = '{"id":"thatGuy","foo":"bar"}';
-#my $decoded = decode_json($jsonString);
-#just dump to file for now  
-#dump both the json and the Data::Dumper
-#my $perlString = Dumper($decoded);
 
 #my $uid = $decoded->{"id"}; #tested
-#open OUT, '>>', '/local/chris_h/houndDog/userData/userLogs.json';
-#print OUT $jsonString; 
-#print OUT "\n"; 
-#close OUT;
-&outputPage($jsonString);
-sub outputPage {
-	print $userData->header(-charset => 'UTF-8');
-	print $_[0];
-}
+
+open OUT, '>>', '/local/chris_h/houndDog/userData/userLogs.json';
+print OUT $jsonString; 
+print OUT "\n"; 
+close OUT;
+
+#&outputPage($jsonString);
+#sub outputPage {
+#	print $userData->header(-charset => 'UTF-8');
+#	print $_[0];
+#}
+
 1;
